d33814635f2792e92f47c5ed8ec62cae1c72ca51
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-06-25 22:14:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  #index = 0
+
+  #expect(position_taken?(board, index)).to be(false)
+
+
+board[index] != " " 
+board[index] == "X" || board[index] == "O" 
+end
